273c8901e02d159b3d2398b3d3a8764b63129d0c
cloud <cloud@instance.com>
2023-03-10 16:58:04 +0530

    deploy to EB

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the React
+          Welcome to the React docker image
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
